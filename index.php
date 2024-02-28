2839bd3749e34cc8e5da9dfd3e7b8ce896e810d3
Kenny Jeremy B <kennybudiono@gmail.com>
2024-02-28 14:34:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tugas 1</title>
+    </head>
+    <body>
+        <?php
+        $nama = 'Kenny Jeremy Budiono';
+        $NIM = '225150401111003';
+        for($i = 1; $i <= 5; $i++){
+            echo "Nama = $nama, NIM = $NIM <br>";
+        }
+        
+        ?>
+    </body>
+</html>
